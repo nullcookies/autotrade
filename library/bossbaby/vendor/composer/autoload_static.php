f6829f66d5cf7869a48f13ee57f00a4f6b6fbbe8
Long Vu <long.vu@sunbytes.vn>
2018-07-26 08:28:04 +0700

    *change the way to load classes

--- library/bossbaby/vendor/composer/autoload_static.php
+++ library/bossbaby/vendor/composer/autoload_static.php
@@ -6,7 +6,8 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit260c8aa5acc5cc8e637c9dd0991124cd
 {
-	public static $files = array (        
+	public static $files = array (
+        // 
     );
 
     public static $prefixLengthsPsr4 = array (
@@ -19,14 +20,15 @@ class ComposerStaticInit260c8aa5acc5cc8e637c9dd0991124cd
     public static $prefixDirsPsr4 = array (
         'BossBaby\\' => 
         array (
-            0 => __DIR__ . '/..' . '/',
+            0 => __DIR__ . '/../..' . '/',
         ),
     );
 
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-
+            $loader->prefixLengthsPsr4 = ComposerStaticInit260c8aa5acc5cc8e637c9dd0991124cd::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit260c8aa5acc5cc8e637c9dd0991124cd::$prefixDirsPsr4;
         }, null, ClassLoader::class);
     }
 }
