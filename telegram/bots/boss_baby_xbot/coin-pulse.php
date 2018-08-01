6c761d4e26ff7337d7f2270d653c60530826bd15
Long Vu <longersoft@gmail.com>
2018-08-01 17:03:34 +0700

    *coin-pulse for Bittrex, fixing

--- telegram/bots/boss_baby_xbot/coin-pulse.php
+++ telegram/bots/boss_baby_xbot/coin-pulse.php
@@ -4,10 +4,6 @@
  * This configuration file is intended to run a list of commands with crontab.
  * Uncommented parameters must be filled
  */
-$str = 'Chú ý coin thay đổi trên <b>Bittrex</b>:
-
-<a href="https://bittrex.com/Market/Index?MarketName=BRX_BTC';
-var_dump($str);die;
 
 if (!defined('STDIN')) die('Access denied.' . "\n");
 
@@ -34,7 +30,7 @@ function run_cron() {
     global $i;
     
     $i++;
-    if ($i > 4) die('FINISHED');
+    if ($i > 3) die('FINISHED');
 
     // Your command(s) to run, pass it just like in a message (arguments supported)
     $commands = [
